d398662a1b1d39a7f54a095dc307fe7678d4f546
Adrian Gois <contato@abgsolucoes.com.br>
2017-02-07 01:16:41 -0300

    cap 6

--- cardapio-movel/www/js/controllers/controllers.js
+++ cardapio-movel/www/js/controllers/controllers.js
@@ -1,7 +1,5 @@
 var app = angular.module('starter.controllers', []);
 
 app.controller('AppCtrl', function($scope, Sessao, $ionicModal, $timeout) {
-	//Nenhuma function
 	$scope.bandeja = Sessao.bandeja;
-	$scope.bandeja = [];
 });
--- cardapio-movel/www/js/controllers/detalhar-controller.js
+++ cardapio-movel/www/js/controllers/detalhar-controller.js
@@ -1,9 +1,13 @@
-app.controller('DetalharCtrl', function($scope, $stateParams, CardapioServices){
+app.controller('DetalharCtrl', function($scope, $stateParams, CardapioServices, Sessao){
 	$scope.item = {};
 
 
 	CardapioServices.getDetalheItem($stateParams.itemID, function(item){
 		$scope.item = item;
 	});
+
+	$scope.inserirPedido = function(quant){
+		Sessao.bandeja.push({item: $scope.item, quantidade : quant, dataHora : new Date()});
+	}
 	
 })
