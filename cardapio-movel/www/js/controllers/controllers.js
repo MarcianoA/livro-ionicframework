f2d8886dd939632e3a09079dd463b313fc2ce200
Adrian Gois <contato@abgsolucoes.com.br>
2017-02-21 02:04:11 -0300

    rerevisado cap 6

--- cardapio-movel/www/js/controllers/controllers.js
+++ cardapio-movel/www/js/controllers/controllers.js
@@ -1,5 +1,23 @@
 var app = angular.module('starter.controllers', []);
 
-app.controller('AppCtrl', function($scope, Sessao, $ionicModal, $timeout) {
+app.controller('AppCtrl', function($scope, Sessao, $timeout, $ionicModal) {
 	$scope.bandeja = Sessao.bandeja;
+	
+	$scope.modal = {};
+	
+	$ionicModal.fromTemplateUrl('bandeja.html', {
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(m){
+		$scope.modal = m;
+	});
+
+	$scope.abreModal = function(){
+		$scope.modal.show();		
+	}
+
+	$scope.fechaModal = function(){
+		$scope.modal.hide();	
+		$scope.input.quant = 1;	
+	}
 });
